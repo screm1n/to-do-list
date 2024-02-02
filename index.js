d687ca8c87bba2ad01bf8b079cf6c7f81d5afeef
Octávio <screminoctavio@gmail.com>
2024-02-02 19:41:12 -0300

    creating db constants

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const port = 3000;
 const db = new pg.Client({
   user: "postgres",
   host: "localhost",
-  database: "permalist"
-  password: "angela"
+  database: "permalist",
+  password: "angela",
   port: 4000,
 });
 db.connect();
@@ -22,19 +22,40 @@ let items = [
 ];
 
 app.get("/", (req, res) => {
+  try {
+    const result = await db.query("SELECT * FROM items ORDER BY id ASC");
+    items = result.rows;
+
   res.render("index.ejs", {
     listTitle: "Today",
     listItems: items,
   });
+} catch (err) {
+  console.log(err);
+}
 });
 
 app.post("/add", (req, res) => {
   const item = req.body.newItem;
-  items.push({ title: item });
-  res.redirect("/");
+  try {
+    await db.query("INSERT INTO items (title) VALUES ($1)", [item]);
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-app.post("/edit", (req, res) => {});
+app.post("/edit", (req, res) => {
+  const item = req.body.updatedItemTitle;
+  const id = req.body.updatedItemId;
+
+  try {
+    await db.query("INSERT items SET title = ($1) WHERE id = $2", [item, id]);
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 app.post("/delete", (req, res) => {});
 
