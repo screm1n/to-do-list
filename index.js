4b30bcfc8ea38371a8869423a2f3792937da8125
Octávio <screminoctavio@gmail.com>
2024-02-02 20:01:00 -0300

    first version of project finished

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
-const port = 3000;
+const port = 4000;
 
 const db = new pg.Client({
   user: "postgres",
   host: "localhost",
   database: "permalist",
   password: "angela",
-  port: 4000,
+  port: 3000,
 });
 db.connect();
 
@@ -21,21 +22,21 @@ let items = [
   { id: 2, title: "Finish homework" },
 ];
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
   try {
     const result = await db.query("SELECT * FROM items ORDER BY id ASC");
     items = result.rows;
 
-  res.render("index.ejs", {
-    listTitle: "Today",
-    listItems: items,
+    res.render("index.ejs", {
+      listTitle: "Today",
+      listItems: items,
   });
 } catch (err) {
   console.log(err);
 }
 });
 
-app.post("/add", (req, res) => {
+app.post("/add", async (req, res) => {
   const item = req.body.newItem;
   try {
     await db.query("INSERT INTO items (title) VALUES ($1)", [item]);
@@ -45,19 +46,27 @@ app.post("/add", (req, res) => {
   }
 });
 
-app.post("/edit", (req, res) => {
+app.post("/edit", async (req, res) => {
   const item = req.body.updatedItemTitle;
   const id = req.body.updatedItemId;
 
   try {
-    await db.query("INSERT items SET title = ($1) WHERE id = $2", [item, id]);
+    await db.query("UPDATE items SET title = ($1) WHERE id = $2", [item, id]);
     res.redirect("/");
   } catch (err) {
     console.log(err);
   }
 });
 
-app.post("/delete", (req, res) => {});
+app.post("/delete", async (req, res) => {
+  const id = req.body.deleteItemId
+  try {
+    await db.query("DELETE FROM items WHERE id = $1", [id]);
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
